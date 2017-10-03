ec22f6fe72d3cfbb431c9de3563e86d059f97733
agilob <info@agilob.net>
2017-10-03 17:40:38 +0100

    Ignore Gg field in Profile

--- models.go
+++ models.go
@@ -10,7 +10,7 @@ type Profile struct {
 	Name            string
 	Www             string
 	Jabber          string
-	Gg              int
+	Gg              int `json:"-"`
 	City            string
 	About           string
 	AuthorGroup     int `json:"author_group"`

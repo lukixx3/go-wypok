e489f0099a17736c9417dd28e4a6afb74cb90c0a
Filip Marek <plfilipmarek@gmail.com>
2017-10-07 14:05:40 +0200

    Add wypok test script (#3)

---
+++ test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+environment_filepath="$(pwd)/.environment_variables"
+required_variables=( "WYKOPAPPKEY" "WYKOPSECRET" "WYKOPCONNECTIONKEY" )
+
+if [ -f "$environment_filepath" ]; then
+    source $environment_filepath
+
+    for v in "${required_variables[@]}"; do
+        if [ -z ${!v} ]; then
+           echo "'$v' has not been set"
+           exit 1
+        fi
+    done
+
+    go test -v
+else
+    echo "You need to create '$environment_filepath' file with"
+    echo "environment variables for test uses i. e.:"
+    echo ""
+    for v in "${required_variables[@]}"; do
+        echo "export $v=xxx"
+    done
+    exit 1
+fi

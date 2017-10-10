dd1a28d4667412b321686ad58ddb195905c98e99
agilob <info@agilob.net>
2017-10-10 11:09:33 +0200

    README: Add list of supported endpoints

--- url_builder.go
+++ url_builder.go
@@ -1,19 +1,23 @@
 package go_wypok
 
 const (
-	LOGIN_URL              = "https://a.wykop.pl/user/login"
+	LOGIN_URL = "https://a.wykop.pl/user/login"
+
 	PROFILE_INDEX          = "https://a.wykop.pl/profile/index/"
 	PROFILE_ENTRIES        = "https://a.wykop.pl/profile/entries/"
 	PROFILE_COMMENTS       = "https://a.wykop.pl/profile/comments/"
 	PROFILE_ENTRY_COMMENTS = "https://a.wykop.pl/profile/entriesComments/"
-	ADD_ENTRY              = "https://a.wykop.pl/entries/add"
-	ENTRY_INDEX            = "https://a.wykop.pl/entries/index/"
-	ENTRY_DELETE           = "https://a.wykop.pl/entries/delete/"
-	ENTRY_VOTE             = "https://a.wykop.pl/entries/vote/"
-	COMMENT_DELETE         = "https://a.wykop.pl/entries/deleteComment/"
-	ENTRIES_OF_A_tAG       = "https://a.wykop.pl/tag/entries/"
-	MAIN_PAGE              = "https://a.wykop.pl/links/promoted/"
-	UPCOMING_PAGE          = "https://a.wykop.pl/links/upcoming/"
+
+	ENTRY_INDEX          = "https://a.wykop.pl/entries/index/"
+	ENTRY_ADD            = "https://a.wykop.pl/entries/add"
+	ENTRY_DELETE         = "https://a.wykop.pl/entries/delete/"
+	ENTRY_VOTE           = "https://a.wykop.pl/entries/vote/"
+	ENTRY_COMMENT_DELETE = "https://a.wykop.pl/entries/deleteComment/"
+
+	ENTRIES_FROM_TAG = "https://a.wykop.pl/tag/entries/"
+
+	MAIN_PAGE     = "https://a.wykop.pl/links/promoted/"
+	UPCOMING_PAGE = "https://a.wykop.pl/links/upcoming/"
 )
 
 func getMainPageUrl() string {
@@ -53,11 +57,11 @@ func getDeleteEntryUrl(entry string) string {
 }
 
 func getDeleteCommentUrl(entryId string, commentId string) string {
-	return COMMENT_DELETE + entryId + "/" + commentId
+	return ENTRY_COMMENT_DELETE + entryId + "/" + commentId
 }
 
 func getAddEntryUrl() string {
-	return ADD_ENTRY
+	return ENTRY_ADD
 }
 
 func getEntryVoteUrl(objectType string, entryId string, commentId string) string {
@@ -65,5 +69,5 @@ func getEntryVoteUrl(objectType string, entryId string, commentId string) string
 }
 
 func getTagEntries(tag string) string {
-	return ENTRIES_OF_A_tAG + tag
+	return ENTRIES_FROM_TAG + tag
 }

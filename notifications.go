123c8d24d2e129a40c532eb74e47e56fed29b2b6
Grzegorz <gmolicki@gmail.com>
2017-10-21 07:48:26 +0200

    Feature/create notifications handlers (#20)
    * Create notifications urls builders and test
    
    * Create notifications handlers, tested manually

---
+++ notifications.go
@@ -0,0 +1,144 @@
+package go_wypok
+
+import (
+	"fmt"
+	"github.com/parnurzeal/gorequest"
+)
+
+const (
+ 	BASE_NOTIFICATIONS_URL = "https://a.wykop.pl/MYWYKOP/%s/appkey/%s/userkey/%s"
+)
+
+type Notification struct {
+	Id           uint          `json:"id"`
+	Author       string        `json:"author"`
+	Avatar       string        `json:"author_avatar"`
+	AvatarMedium string        `json:"author_avatar_med"`
+	AvatarLow    string        `json:"author_avatar_lo"`
+	AvatarBig    string        `json:"author_avatar_big"`
+	AuthorGroup  string        `json:"author_group"`
+	AuthorSex    string        `json:"author_sex"`
+	Date         WypokShitDate `json:"date"`
+	Content      string        `json:"body"`
+	Type         string        `json:"type"`
+	Url          string        `json:"url"`
+}
+
+func (wh *WykopHandler) getNotifications(page uint) (notifications []Notification, wykopError *WykopError) {
+	urlAddress := getNotificationsUrl(page, wh)
+
+	_, responseBody, _ := gorequest.New().Post(urlAddress).
+									Set(contentType, mediaTypeFormType).
+									Set(apiSignHeader, wh.hashRequest(urlAddress)).
+									End()
+
+	wykopError = wh.getObjectFromJson(responseBody, &notifications)
+
+	return
+}
+
+func (wh *WykopHandler) getNotificationsCount() (counter uint, wykopError *WykopError) {
+	urlAddress := getNotificationsCountUrl(wh)
+
+	_, responseBody, _ := gorequest.New().Post(urlAddress).
+									Set(contentType, mediaTypeFormType).
+									Set(apiSignHeader, wh.hashRequest(urlAddress)).
+									End()
+
+	wykopError = wh.getObjectFromJson(responseBody, &counter)
+
+	return
+}
+
+func (wh *WykopHandler) getHashTagsNotifications(page uint) (notifications []Notification, wykopError *WykopError) {
+	urlAddress := getHashTagsNotificationsUrl(page, wh)
+
+	_, responseBody, _ := gorequest.New().Post(urlAddress).
+									Set(contentType, mediaTypeFormType).
+									Set(apiSignHeader, wh.hashRequest(urlAddress)).
+									End()
+
+	wykopError = wh.getObjectFromJson(responseBody, &notifications)
+
+	return
+}
+
+func (wh *WykopHandler) getHashTagsNotificationsCount() (counter uint, wykopError *WykopError) {
+	urlAddress := getHashTagsNotificationsCountUrl(wh)
+
+	_, responseBody, _ := gorequest.New().Post(urlAddress).
+									Set(contentType, mediaTypeFormType).
+									Set(apiSignHeader, wh.hashRequest(urlAddress)).
+									End()
+
+	wykopError = wh.getObjectFromJson(responseBody, &counter)
+
+	return
+}
+
+func (wh *WykopHandler) readNotifications() (wykopError *WykopError) {
+	urlAddress := getReadNotificationsUrl(wh)
+
+	_, responseBody, _ := gorequest.New().Post(urlAddress).
+									Set(contentType, mediaTypeFormType).
+									Set(apiSignHeader, wh.hashRequest(urlAddress)).
+									End()
+
+	wykopError = wh.getObjectFromJson(responseBody, nil)
+
+	return
+}
+
+func (wh *WykopHandler) readHashTagsNotifications() (wykopError *WykopError) {
+	urlAddress := getReadHashTagsNotificationsUrl(wh)
+
+	_, responseBody, _ := gorequest.New().Post(urlAddress).
+									Set(contentType, mediaTypeFormType).
+									Set(apiSignHeader, wh.hashRequest(urlAddress)).
+									End()
+
+	wykopError = wh.getObjectFromJson(responseBody, nil)
+
+	return
+}
+
+func (wh *WykopHandler) readNotification(notificationId uint) (result bool, wykopError *WykopError) {
+	urlAddress := getMarkNotificationAsReadUrl(notificationId, wh)
+
+	_, responseBody, _ := gorequest.New().Post(urlAddress).
+									Set(contentType, mediaTypeFormType).
+									Set(apiSignHeader, wh.hashRequest(urlAddress)).
+									End()
+	result = false
+	wykopError = wh.getObjectFromJson(responseBody, &result)
+
+	return
+}
+
+func getNotificationsUrl(page uint, wh *WykopHandler) string {
+	return fmt.Sprintf(BASE_NOTIFICATIONS_URL, "Notifications", wh.appKey, wh.authResponse.Userkey) + fmt.Sprintf("/page/%d", page)
+}
+
+func getNotificationsCountUrl(wh *WykopHandler) string {
+	return fmt.Sprintf(BASE_NOTIFICATIONS_URL, "NotificationsCount", wh.appKey, wh.authResponse.Userkey)
+}
+
+func getHashTagsNotificationsUrl(page uint, wh *WykopHandler) string {
+	return fmt.Sprintf(BASE_NOTIFICATIONS_URL, "HashTagsNotifications", wh.appKey, wh.authResponse.Userkey) + fmt.Sprintf("/page/%d", page)
+}
+
+func getHashTagsNotificationsCountUrl(wh *WykopHandler) string {
+	return fmt.Sprintf(BASE_NOTIFICATIONS_URL, "HashTagsNotificationsCount", wh.appKey, wh.authResponse.Userkey)
+}
+
+func getReadNotificationsUrl(wh *WykopHandler) string {
+	return fmt.Sprintf(BASE_NOTIFICATIONS_URL, "ReadNotifications", wh.appKey, wh.authResponse.Userkey)
+}
+
+func getReadHashTagsNotificationsUrl(wh *WykopHandler) string {
+	return fmt.Sprintf(BASE_NOTIFICATIONS_URL, "ReadHashTagsNotifications", wh.appKey, wh.authResponse.Userkey)
+}
+
+func getMarkNotificationAsReadUrl(notificationId uint, wh *WykopHandler) string {
+	return fmt.Sprintf(BASE_NOTIFICATIONS_URL, fmt.Sprintf("MarkAsReadNotification/%d", notificationId), wh.appKey, wh.authResponse.Userkey)
+}
---
+++ notifications_test.go
@@ -0,0 +1,58 @@
+package go_wypok
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+// As notifications are nearly impossible to be tested automatically, you need a help everytime you run tests.
+func TestWykopHandler_TestNotification(t *testing.T) {
+	teardownTestCase := setupTestCase(t)
+	defer teardownTestCase(t)
+
+	wh.LoginToWypok()
+
+	//success, err := wh.readNotification(9299079331)
+	//
+	//fmt.Println("ReadNotification success: ", success)
+	//fmt.Println("ReadNotification err: ", err)
+
+	//notifications, err := wh.getNotifications(1)
+	//fmt.Println("notifications: ", notifications)
+	//fmt.Println("notificationsErr: ", err)
+	//
+	//notifications, err = wh.getHashTagsNotifications(1)
+	//fmt.Println("#notifications: ", notifications)
+	//fmt.Println("#notificationsErr: ", err)
+	//
+	//counter, err := wh.getNotificationsCount()
+	//fmt.Println("NotificationsCounter: ", counter)
+	//fmt.Println("NotificationsErr: ", err)
+	//
+	//counter, err = wh.getHashTagsNotificationsCount()
+	//fmt.Println("#NotificationsCounter: ", counter)
+	//fmt.Println("#NotificationsErr: ", err)
+}
+
+func TestWykopHandler_getNotificationsUrls(t *testing.T) {
+	wh := WykopHandler{}
+	wh.authResponse.Userkey = "123"
+	wh.appKey = "abc"
+
+	notificationsUrl := getNotificationsUrl(1, &wh)
+	notificationsCountUrl := getNotificationsCountUrl(&wh)
+	hashTagsNotificationsUrl := getHashTagsNotificationsUrl(1, &wh)
+	hashTagsNotificationsCountUrl := getHashTagsNotificationsCountUrl(&wh)
+	readNotificationsUrl := getReadNotificationsUrl(&wh)
+	readHashTagsNotificationsUrl := getReadHashTagsNotificationsUrl(&wh)
+	markNotificationAsReadUrl := getMarkNotificationAsReadUrl(1, &wh)
+
+	assert.Equal(t, notificationsUrl, "https://a.wykop.pl/MYWYKOP/Notifications/appkey/abc/userkey/123/page/1")
+	assert.Equal(t, notificationsCountUrl, "https://a.wykop.pl/MYWYKOP/NotificationsCount/appkey/abc/userkey/123")
+	assert.Equal(t, hashTagsNotificationsUrl, "https://a.wykop.pl/MYWYKOP/HashTagsNotifications/appkey/abc/userkey/123/page/1")
+	assert.Equal(t, hashTagsNotificationsCountUrl, "https://a.wykop.pl/MYWYKOP/HashTagsNotificationsCount/appkey/abc/userkey/123")
+	assert.Equal(t, readNotificationsUrl, "https://a.wykop.pl/MYWYKOP/ReadNotifications/appkey/abc/userkey/123")
+	assert.Equal(t, readHashTagsNotificationsUrl, "https://a.wykop.pl/MYWYKOP/ReadHashTagsNotifications/appkey/abc/userkey/123")
+	assert.Equal(t, markNotificationAsReadUrl, "https://a.wykop.pl/MYWYKOP/MarkAsReadNotification/1/appkey/abc/userkey/123")
+}
+
